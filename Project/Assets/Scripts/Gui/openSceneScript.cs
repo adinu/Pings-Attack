373a67de5cb5936ff02c686550a73d28e4f9eb1f
michrabi <michalrabinowitch@michals-MacBook-Pro.local>
2015-01-01 18:47:55 +0200

    scenes updates

--- Project/Assets/Scripts/Gui/openSceneScript.cs
+++ Project/Assets/Scripts/Gui/openSceneScript.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class openSceneScript : MonoBehaviour {
 
+	public AudioSource bearSleeping;
+	public int TimeInSecondsBeforNExtSceneStarts;
+
 	// Use this for initialization
 	void Start () {
-	
+		bearSleeping.Play ();
+
+
 	}
+	
+	IEnumerator suspendNextScene()
+	{
+		yield return new WaitForSeconds(TimeInSecondsBeforNExtSceneStarts);
+		Application.LoadLevel(1);
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
+		suspendNextScene ();
 	
 	}
 }
