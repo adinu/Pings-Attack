5d3bbe17ed5cc3831658f052abd36261db1363bd
SergeOxe <sergeoxe@gmail.com>
2015-01-01 22:36:10 +0200

    Infrastructure for build easy waves
    Prefab createWave
    and prefab CreateWaves

---
+++ Project/Assets/createEnemeiesWave.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createEnemeiesWave : MonoBehaviour {
+	public GameObject[] enemies;
+	public Vector3[] positionValues;
+	public float[] timeUntilNextEnemy;
+	public float timeUntilFirstWave;
+	
+	void Start (){
+		StartCoroutine (CreateWave ());
+	}
+	
+	IEnumerator CreateWave ()
+	{
+		yield return new WaitForSeconds (timeUntilFirstWave);
+		for (int i = 0; i < enemies.Length; i++){
+			Instantiate (enemies[i], positionValues[i], Quaternion.identity);
+			yield return new WaitForSeconds (timeUntilNextEnemy[i]);
+		}
+	}
+}
